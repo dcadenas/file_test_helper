36e143e2a4966eabef658f4c7c99547e282edfa6
Daniel Cadenas <dcadenas@gmail.com>
2009-05-13 00:19:18 -0300

    Added ability to change base dir other than the temp dir

---
+++ lib/file_test_helper.rb
@@ -0,0 +1,81 @@
+require 'tmpdir'
+
+module FileTestHelper
+  include FileUtils
+
+  #Specify the files you need to create in a hash.
+  #The keys of the hash represent the file path and the value represents it's content.
+  #Examples:
+  #* {'a dir/a file' => 'content'} creates the 'a dir' directory containing an 'a file' file which content is 'content'.
+  #* {'another dir/' => ''} creates an empty 'another dir' directory. The value must be an empty string.
+  #Optionally you can specify a different base directory (other than the default tmp) to use as a base directory.
+  #Example:
+  #* 'stylesheets', 'file.txt' => "" will create the file.txt file inside the existent stylesheets directory. The stylesheets directory won't be deleted
+  def with_files(files_with_contents = {})
+    with_files_in_directory(nil, files_with_contents) do
+      yield
+    end
+  end
+
+  def with_files_in_directory(base_dir, files_with_contents)
+    initial_directory = current_directory
+    raise ArgumentError, "The base directory '#{base_dir}' does not exist." if base_dir && !File.exist?(base_dir)
+    working_directory = base_dir || create_working_directory
+
+    begin
+      create_files_in_working_directory(working_directory, files_with_contents)
+      yield
+    ensure
+      cd initial_directory
+      if base_dir.nil?
+        remove_dir(working_directory) if File.exist?(working_directory)
+      else
+        remove_files(base_dir, files_with_contents)
+      end
+    end
+  end
+
+  private
+  def current_directory
+    Dir.pwd
+  end
+
+  def create_working_directory
+    working_directory = File.join(Dir.tmpdir, "__test_dir__#{Process.pid}")
+    mkpath(working_directory)
+    return working_directory 
+  end
+
+  def create_files_in_working_directory(working_directory, files_with_contents)
+    cd working_directory
+
+    return unless files_with_contents
+    files_with_contents.each do |path, file_contents|
+      raise ArguemntError, 'A path is not allowed to start with /' if path =~ /^\//
+        raise ArgumentError, 'A path is not allowed to contain ..' if path =~ /\.\./
+
+        dir, file = path.scan(/(.*[\/])?([^\/]*$)/)[0] 
+      mkpath dir unless(dir.nil? or dir.empty?)
+
+      unless(file.nil? or file.empty?)
+        File.open(path, 'w') do |f|
+          f << file_contents unless file_contents == nil
+        end
+      else
+        raise ArgumentError, 'File content can only be set to files' unless file_contents.nil? or file_contents.empty?
+      end
+    end    
+  end
+
+  def remove_files(base_dir, files_with_contents)
+    initial_dir = Dir.pwd
+    cd base_dir
+    begin
+      files_with_contents.each do |path, file_contents|
+        remove_dir(path)
+      end
+    ensure
+      cd initial_dir
+    end
+  end
+end
--- spec/file_test_helper_spec.rb
+++ spec/file_test_helper_spec.rb
@@ -1,63 +1,63 @@
 require File.dirname(__FILE__) + "/spec_helper"
 require 'rubygems'
 require 'spec'
-require 'filetesthelper'
+require 'file_test_helper'
 include FileTestHelper
 
-parameters = {
-  'nil' => nil,
-  'an empty hash' => {},
-  'a hash with only one file' => {'file.txt' => 'file content'}
-}
+describe FileTestHelper do
+  parameters = {
+    'nil' => nil,
+    'an empty hash' => {},
+    'a hash with only one file' => {'file.txt' => 'file content'}
+  }
 
-parameters.each do |parameter_name, parameter|
-  describe FileTestHelper, "when the hash paramater is #{parameter_name}" do
-    it 'should execute the block parameter' do
-      block_runned = false
-      with_files(parameter) {block_runned = true}
-      block_runned.should be_true
-    end
-  
-    it 'should not use the current directory' do
-      initial_directory = Dir.pwd
-      with_files(parameter) do
-        Dir.pwd.should_not == initial_directory
+  parameters.each do |parameter_name, parameter|
+    describe "when the hash paramater is #{parameter_name}" do
+      it 'should execute the block parameter' do
+        block_runned = false
+        with_files(parameter) {block_runned = true}
+        block_runned.should be_true
       end
-      Dir.pwd.should == initial_directory
-    end
-  
-    it 'should start in an empty working directory' do
-      with_files(parameter) do
-        Dir.glob('**/*').size == 0
+
+      it 'should not use the current directory' do
+        initial_directory = Dir.pwd
+        with_files(parameter) do
+          Dir.pwd.should_not == initial_directory
+        end
+        Dir.pwd.should == initial_directory
       end
-    end
-  
-    it 'should delete the working directory and its contents after running the block' do
-      working_directory_path = ''
-      with_files(parameter) do
-        working_directory_path = Dir.pwd
+
+      it 'should start in an empty working directory' do
+        with_files(parameter) do
+          Dir.glob('**/*').size == 0
+        end
       end
-      working_directory_path.should_not be_empty
-      File.exist?(working_directory_path).should be_false
+
+      it 'should delete the working directory and its contents after running the block' do
+        working_directory_path = ''
+        with_files(parameter) do
+          working_directory_path = Dir.pwd
+        end
+        working_directory_path.should_not be_empty
+        File.exist?(working_directory_path).should be_false
+      end
     end
   end
-end
 
-describe FileTestHelper do
   it 'should create the files and directories that were specified in a hash' do
     files_to_create = {'a directory/a file.txt' => '', 'a directory/another file.rb' => '', 'an_empty_directory/' => '', 'a_file' => ''}
     with_files(files_to_create) do
       files_to_create.each_key{|created_file| File.exist?(created_file).should be_true}
     end
   end
-  
+
   it 'should be possible to define the content of created files' do
     with_files('filea' => 'content of filea', 'fileb' => 'content of fileb') do
       File.read('filea').should == 'content of filea'
       File.read('fileb').should == 'content of fileb'
     end
   end
-  
+
   it 'should interpret a path with an ending / as a directory' do
     with_files('this is a directory/another dir/' => '') do
       File.directory?('this is a directory/another dir').should be_true
@@ -67,16 +67,47 @@ describe FileTestHelper do
   it 'should throw an error if trying to set file content to a directory' do
     lambda { with_files('directory/' => 'imposible content, directories are not files') {}}.should raise_error
   end
-  
+
   it 'should throw an error if a path which starts with the "/" character is specified' do
     lambda { with_files('/directory/filea' => 'content of filea') {}}.should raise_error
   end
-  
+
   it 'should throw an error if a path uses ".."' do
     lambda { with_files('../../dir/filea' => 'content of filea') {}}.should raise_error
   end
-  
+
   it 'should not throw an exception when the hash parameter is nil' do
     lambda { with_files(nil) {}}.should_not raise_error
+  end
+
+  describe "with base_dir" do
+    it "should use the specified base dir instead of the default tmp dir" do
+      initial_directory = Dir.pwd
+      with_files_in_directory(initial_directory, 'somefile.txt' => '') do
+        Dir.pwd.should == initial_directory
+      end
+      Dir.pwd.should == initial_directory
+    end
+
+    it "should not delete the base dir" do
+      base_dir = FileUtils.mkpath("delete_this_dir")
+      begin
+        with_files_in_directory(base_dir, 'somefile.txt' => '') {}
+        File.exist?(base_dir).should be_true
+      ensure
+        FileUtils.remove_dir(base_dir)
+      end
+    end
+
+    it "should delete the created files" do
+      with_files_in_directory(Dir.pwd, 'somefile.txt' => '') {}
+      File.exist?('somefile.txt').should be_false
+    end
+
+    it "should throw an exception if the specified base dir doesn't exist" do
+      lambda{
+        with_files_in_directory('this_dir_does_not_exist', 'somefile.txt' => '') {}
+      }.should raise_error(ArgumentError)
+    end
   end
 end
