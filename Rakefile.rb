c786c942daccb97d6e0b953d7834afdb7d6320d0
Daniel Cadenas <dcadenas@gmail.com>
2009-05-12 21:56:13 -0300

    Updated gemspec and using jeweler

--- Rakefile.rb
+++ Rakefile.rb
@@ -5,55 +5,26 @@ require "rake/clean"
 require 'rake/rdoctask'
 require 'rake/gempackagetask'
 require 'spec/rake/spectask'
-
-NAME = 'filetesthelper'
-PKGVERSION = '0.9.1'
-is_windows = (PLATFORM =~ /win32|cygwin/)
-SUDO = is_windows ? '' : 'sudo'
-README = "README.txt"
-
-
-##############################################################################
-# Packaging & Installation
-##############################################################################
-
-CLEAN.include ["pkg", "coverage", "doc"]
-
-spec = Gem::Specification.new do |s| 
-  s.name = NAME
-  s.version = PKGVERSION
-  s.author = "Daniel Cadenas Nión"
-  s.email = "filetesthelper-devel@rubyforge.org"
-  s.homepage = "http://rubyforge.org/projects/filetesthelper/"
-  s.platform = Gem::Platform::RUBY
-  s.summary = "A simple helper aimed at reducing the setup effort needed to create directories, files and file content in integration test cases."
-  s.description = s.summary
-  s.files = Dir["{spec,lib}/**/*"] + [README, "Rakefile.rb"]
-  s.has_rdoc = true
-  s.extra_rdoc_files = [README]
-  s.require_path = 'lib'
-  s.autorequire = NAME
-#  s.require_paths << ['lib']
+ 
+begin
+  require 'jeweler'
+  Jeweler::Tasks.new do |gemspec|
+    gemspec.name = "filetesthelper"
+    gemspec.summary = "A simple helper aimed at reducing the setup effort needed to create directories, files and file content in integration test cases."
+    gemspec.email = "dcadenas@gmail.com"
+    gemspec.homepage = "http://rubyforge.org/projects/filetesthelper/"
+    gemspec.description = gemspec.summary
+    gemspec.authors = ["Daniel Cadenas Nión"]
+    gemspec.version = '0.9.1'
+    gemspec.platform = Gem::Platform::RUBY
+  #gemspec.files = Dir["{spec,lib}/**/*"] + [README, "Rakefile.rb"]
+    gemspec.require_path = 'lib'
+    gemspec.autorequire = gemspec.name
+  end
+rescue LoadError
+  puts "Jeweler not available. Install it with: sudo gem install technicalpickles-jeweler -s http://gems.github.com"
 end
 
-Rake::GemPackageTask.new(spec) do |pkg| 
-  pkg.gem_spec = spec
-end 
-
-desc "Run :package and install the resulting .gem"
-task :install => [:clean, :specs_with_rcov, :rdoc, :package] do
-  Gem::GemRunner.new.run(['install', "pkg/#{NAME}-#{PKGVERSION}.gem", '--no-rdoc', '--no-ri'])
-end
-
-desc "Run :clean and uninstall the .gem"
-task :uninstall => :clean do
-  Gem::GemRunner.new.run(['uninstall', NAME])
-end
-
-
-##############################################################################
-# rSpec & rcov
-##############################################################################
 
 Spec::Rake::SpecTask.new('specs_with_rcov') do |t|
   t.ruby_opts << '-rubygems'
@@ -63,16 +34,4 @@ Spec::Rake::SpecTask.new('specs_with_rcov') do |t|
   t.rcov_opts = ['--text-summary']
 end
 
-
-##############################################################################
-# Documentation
-##############################################################################
-
-Rake::RDocTask.new(:rdoc) do |rd|  
-  rd.rdoc_files = [README] + Dir['lib/**/*']
-  rd.rdoc_dir = 'doc' 
-  rd.title    = NAME
-  rd.options << "-SNm#{README}"
-end
-
-task :default => [:specs_with_rcov, :rdoc]
+task :default => [:specs_with_rcov]
---
+++ filetesthelper.gemspec
@@ -0,0 +1,31 @@
+# -*- encoding: utf-8 -*-
+
+Gem::Specification.new do |s|
+  s.name = %q{filetesthelper}
+  s.version = "0.10.0"
+
+  s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
+  s.authors = ["Daniel Cadenas Ni\303\263n"]
+  s.autorequire = %q{filetesthelper}
+  s.date = %q{2009-05-12}
+  s.description = %q{A simple helper aimed at reducing the setup effort needed to create directories, files and file content in integration test cases.}
+  s.email = %q{dcadenas@gmail.com}
+  s.extra_rdoc_files = ["README.rdoc"]
+  s.files = ["Rakefile.rb", "README.rdoc", "VERSION.yml", "lib/filetesthelper.rb", "spec/FileTestHelper_spec.rb", "spec/spec.opts", "spec/spec_helper.rb"]
+  s.has_rdoc = true
+  s.homepage = %q{http://rubyforge.org/projects/filetesthelper/}
+  s.rdoc_options = ["--inline-source", "--charset=UTF-8"]
+  s.require_paths = ["lib"]
+  s.rubygems_version = %q{1.3.1}
+  s.summary = %q{A simple helper aimed at reducing the setup effort needed to create directories, files and file content in integration test cases.}
+
+  if s.respond_to? :specification_version then
+    current_version = Gem::Specification::CURRENT_SPECIFICATION_VERSION
+    s.specification_version = 2
+
+    if Gem::Version.new(Gem::RubyGemsVersion) >= Gem::Version.new('1.2.0') then
+    else
+    end
+  else
+  end
+end
