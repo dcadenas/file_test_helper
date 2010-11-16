373e7b38fe7f491c57d83df30281c08244053c2c
Daniel Cadenas <dcadenas@gmail.com>
2010-11-16 21:34:36 -0200

    Removed jeweler dependency

--- Rakefile.rb
+++ Rakefile.rb
@@ -6,26 +6,6 @@ require "rake/clean"
 require 'rake/rdoctask'
 require 'rake/gempackagetask'
 require 'spec/rake/spectask'
- 
-begin
-  require 'jeweler'
-  Jeweler::Tasks.new do |gemspec|
-    gemspec.name = "filetesthelper"
-    gemspec.summary = "A simple helper aimed at reducing the setup effort needed to create directories, files and file content in integration test cases."
-    gemspec.email = "dcadenas@gmail.com"
-    gemspec.homepage = "http://rubyforge.org/projects/filetesthelper/"
-    gemspec.description = gemspec.summary
-    gemspec.authors = ["Daniel Cadenas Nión"]
-    gemspec.version = '1.0.1'
-    gemspec.platform = Gem::Platform::RUBY
-  #gemspec.files = Dir["{spec,lib}/**/*"] + [README, "Rakefile.rb"]
-    gemspec.require_path = 'lib'
-    gemspec.autorequire = gemspec.name
-  end
-rescue LoadError
-  puts "Jeweler not available. Install it with: sudo gem install technicalpickles-jeweler -s http://gems.github.com"
-end
-
 
 Spec::Rake::SpecTask.new('specs_with_rcov') do |t|
   t.ruby_opts << '-rubygems'
